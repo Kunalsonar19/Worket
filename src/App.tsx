98cb0bf2b13c112731bf23bc19b7848d42512686
Kunalsonar19 <kunalwankhede958@gmail.com>
2025-08-24 16:56:15 +0530

    New Commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/verify" element={<Verify />} />
+          <Route path="/Verify" element={<Verify />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
