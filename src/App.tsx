40d869c768d531bbadb12fd9ebcce5880d673c14
Kunalsonar19 <kunalwankhede958@gmail.com>
2025-08-24 16:39:24 +0530

    New Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Index from "./pages/Index";
+import Index from "./pages/Index";
 import Verify from "./pages/Verify";
 import NotFound from "./pages/NotFound";
 
@@ -16,6 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Index />} />
           <Route path="/verify" element={<Verify />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
